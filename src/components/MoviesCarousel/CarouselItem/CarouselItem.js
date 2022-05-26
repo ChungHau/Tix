2b8c67f5698fe4e741c64ce519a8118eab21cfa9
ChungHau <kidodo212@gmail.com>
2022-05-26 17:09:47 +0700

    Complete MovieCarousel

--- src/components/MoviesCarousel/CarouselItem/CarouselItem.js
+++ src/components/MoviesCarousel/CarouselItem/CarouselItem.js
@@ -1,17 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { PlayIcon } from '@heroicons/react/outline';
+import './CarouselItem.css';
+import Modal from '../../Modal/Modal';
 const CarouselItem = (props) => {
+  const [show, setShow] = useState(false);
   return (
-    <div className='carousel__item'>
-      <div className='carousel__img'>
-        <img src={props.imageSrc} alt='' width='100%' />
+    <>
+      <div className='carousel__item relative'>
+        <div className='carousel__img'>
+          <img src={props.imageSrc} alt='' width='100%' />
+        </div>
+        <div className='playBtn__overplay hidden md:block absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 opacity-0 '>
+          <button onClick={() => setShow(true)}>
+            <PlayIcon className='h-20 w-20 text-indigo-500' />
+          </button>
+        </div>
       </div>
-      <div className='playBtn__overplay'>
-        <a>
-          <img />
-        </a>
-      </div>
-    </div>
+      <Modal show={show} onClose={() => setShow(false)}>
+        <iframe
+          width={560}
+          height={315}
+          src='https://www.youtube.com/embed/mVsJXiI60a0'
+          title='YouTube video player'
+          frameBorder={0}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        />
+      </Modal>
+    </>
   );
 };
 
--- src/components/MoviesCarousel/index.js
+++ src/components/MoviesCarousel/index.js
@@ -3,6 +3,7 @@ import '../../../node_modules/slick-carousel/slick/slick.css';
 import '../../../node_modules/slick-carousel/slick/slick-theme.css';
 import CarouselItem from './CarouselItem/CarouselItem';
 import './index.css';
+
 function importAll(r) {
   let images = {};
   r.keys().forEach((item, index) => {
@@ -11,8 +12,18 @@ function importAll(r) {
   return images;
 }
 const images = importAll(
-  require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/)
+  require.context('../../assets/images/', false, /.(png)$/)
 );
+
+const carouselImg = Object.keys(images)
+  .filter((key) => key.includes('imageCarousel'))
+  .reduce((obj, key) => {
+    return Object.assign(obj, {
+      [key]: images[key],
+    });
+  }, {});
+
+console.log(carouselImg);
 
 const MoviesCarousel = () => {
   const settings = {
@@ -29,38 +40,14 @@ const MoviesCarousel = () => {
   return (
     <div className='moviesCarousel mt-24'>
       <Slider {...settings}>
-        {images &&
-          numberImagesArr.map((num) => (
-            <CarouselItem
-              key={Math.random()}
-              imageSrc={images[`imageCarousel${num}.png`]}
-            />
-          ))}
+        {numberImagesArr.map((num) => (
+          <CarouselItem
+            key={Math.random()}
+            imageSrc={carouselImg[`imageCarousel${num}.png`]}
+          />
+        ))}
       </Slider>
     </div>
-
-    // <div className='bg-white'>
-    //   <Slider {...settings}>
-    //     <div>
-    //       <h3>1</h3>
-    //     </div>
-    //     <div>
-    //       <h3>2</h3>
-    //     </div>
-    //     <div>
-    //       <h3>3</h3>
-    //     </div>
-    //     <div>
-    //       <h3>4</h3>
-    //     </div>
-    //     <div>
-    //       <h3>5</h3>
-    //     </div>
-    //     <div>
-    //       <h3>6</h3>
-    //     </div>
-    //   </Slider>
-    // </div>
   );
 };
 
